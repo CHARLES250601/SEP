21d79b545628b5c6f72b7917be5243ec473a42fe
novand999 <novandkensela@gmail.com>
2022-03-03 20:28:10 +0700

    commit novand1

---
+++ laravel/resources/views/login.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>LOGIN</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="/signin.css">
+  </head>
+
+  <body class="text-center">
+
+    @if ($errors->any())
+    <div class="error">
+        <ul>
+            @foreach ($errors->all() as $err)
+                <li class="alert alert-danger">{{$err}}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+        <main class="form-signin">
+            @csrf
+            <form action="" method="POST">
+                <img class="mb-4" src="login.jpg" alt="" width="100" height="100">
+                <h1 class="h3 mb-3 fw-normal">Please Login</h1>
+
+                <div class="form-floating">
+                <input type="email" class="form-control" name="pemakai_email" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                <input type="password" class="form-control"  name="pemakai_password" placeholder="Password">
+                <label for="floatingPassword">Password</label>
+                </div>
+
+                <div class="checkbox mb-3">
+                <label>
+                    <input type="checkbox" value="remember-me"> Remember me
+                </label>
+                <br>
+                <br>
+                <button class="w-100 btn btn-lg btn-success" type="submit">Login</button>
+                <br>
+                <br>
+                <button class="w-100 btn btn-lg btn-warning" type="submit">Register</button>
+            </form>
+        </main>
+
+@if (Session::has('pesan'))
+    <div class="alert alert-danger">{{ Session::get('pesan') }}</div>
+@endif
+
+</body>
+</html>
---
+++ laravel/resources/views/pengunjung.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('konten')
+    <h1>SELAMAT DATANG DI HALAMAN PENGUNJUNG</h1>
+@endsection
